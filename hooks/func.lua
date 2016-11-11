70e05cf3ec35f99a7733992794ba63cd4d62a701
Alek Fleischer <thecrazyal02@gmail.com>
2016-11-11 15:31:09 -0800

    Actual randomization!!

---
+++ hooks/func.lua
@@ -0,0 +1,6 @@
+-- Good old Set!
+function Set (list)
+	local set = {}
+	for _, l in ipairs(list) do set[l] = true end
+	return set
+end
--- hooks/love_keypressed_start.lua
+++ hooks/love_keypressed_start.lua
@@ -1,5 +1,9 @@
 if nodialog and editingroomtext == 0 and editingroomname == false and (state == 1) and (key == "r") and (love.keyboard.isDown("l" .. ctrl) or love.keyboard.isDown("r" .. ctrl)) then
+	starttiles = Set {6,7,53,54,55,56,57,58,59,60,61,62,65,66,67,68,69,70,71,72,73,74}
+	endtiles = {6,7,53,54,55,56,57,58,59,60,61,62,65,66,67,68,69,70,71,72,73,74}
 	for i=1,1200 do
-		roomdata[roomy][roomx][i] = math.random(0, 1198)
+		if starttiles[roomdata[roomy][roomx][i]] then
+			roomdata[roomy][roomx][i] = endtiles[math.random(#endtiles)]
+		end
 	end
 end
