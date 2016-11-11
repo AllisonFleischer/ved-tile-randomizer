491bff695d9944ae5787d79e81f5da4dd71a5640
Alek Fleischer <thecrazyal02@gmail.com>
2016-11-11 14:18:06 -0800

    Combo now fills whole room w/ random tiles
    Need restrictions!

--- hooks/love_keypressed_start.lua
+++ hooks/love_keypressed_start.lua
@@ -1,3 +1,5 @@
 if nodialog and editingroomtext == 0 and editingroomname == false and (state == 1) and (key == "r") and (love.keyboard.isDown("l" .. ctrl) or love.keyboard.isDown("r" .. ctrl)) then
-  roomdata[roomy][roomx][3] = 40 --Debug function
+  for i=1,1200 do
+    roomdata[roomy][roomx][i] = math.random(0, 1198)
+  end
 end
--- info.lua
+++ info.lua
@@ -7,7 +7,6 @@ t.version = "1.0"
 t.minimumved = "b14"
 t.description = [[
 Use Ctrl+R to randomize tiles.
-Tiles are hardcoded for now;
-customization is planned for release.
+Tiles are hardcoded for now; customization is planned for release.
 ]]
 t.overrideinfo = false
