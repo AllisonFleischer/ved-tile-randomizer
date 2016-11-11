8e8ae5921b95aa8712b9b255341d5cf3a03cda80
Alek Fleischer <thecrazyal02@gmail.com>
2016-11-10 16:11:24 -0800

    Initial check-in

---
+++ info.lua
@@ -0,0 +1,12 @@
+t = ...
+
+t.shortname = "Randomizer"
+t.longname = "Tile Randomizer"
+t.author = "crazya"
+t.version = "1.0"
+t.minimumved = "b14"
+t.description = [[
+beep boop
+write something useful here later
+]]
+t.overrideinfo = false
