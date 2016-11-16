eb018165591bcd008c93aea11cd2cfc7701de6ce
Alek Fleischer <thecrazyal02@gmail.com>
2016-11-15 17:23:20 -0800

    Add lots of comments

--- hooks/love_keypressed_start.lua
+++ hooks/love_keypressed_start.lua
@@ -1,19 +1,21 @@
 if nodialog and editingroomtext == 0 and editingroomname == false and (state == 1) and (key == "r") and (love.keyboard.isDown("l" .. ctrl) or love.keyboard.isDown("r" .. ctrl)) then
 
+	--Turn on Manual Mode (since randomization will naturally mess up tilesets)
 	levelmetadata[(roomy)*20 + (roomx+1)].auto2mode = 0
 	levelmetadata[(roomy)*20 + (roomx+1)].directmode = 1
 
+	--Create the matrix
 	mt = {}
-
 	for i = 1, 2 do
 		mt[i] = {}
-
 		for j = 1, 2 do
 				mt[i][j] = 0
 		end
 	end
 
 	--[[
+	mt: Array of "bundles"
+	mt[x]: Bundle of tile replacements
 	mt[x][1]: Set of tile numbers to replace
 	mt[x][2]: Normal array of tile numbers to replace with
 	]]
@@ -21,9 +23,10 @@ if nodialog and editingroomtext == 0 and editingroomname == false and (state == 
 	-- Here's the biggest hardcoded nonsense to change for 1.0
 	mt[1][1] = Set {6,7,53,54,55,56,57,58,59,60,61,62,65,66,67,68,69,70,71,72,73,74}
 	mt[1][2] = {6,7,53,54,55,56,57,58,59,60,61,62,65,66,67,68,69,70,71,72,73,74}
-	mt[2][1] = Set {0}
-	mt[2][2] = {20}
+	mt[2][1] = Set {880,881,882,883,884,920,921,922,923,924,960,961,962}
+	mt[2][2] = {880,881,882,883,884,920,921,922,923,924,960,961,962}
 
+	--Finally, randomize the tiles!
 	for mtCount = 1,#mt do
 		for i=1,1200 do
 			if mt[mtCount][1][roomdata[roomy][roomx][i]] then
